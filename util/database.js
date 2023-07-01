d3333f51fa59ca5b73e6902eafff33801fab5330
HyperZep <ujjwal@hyperzepinfotech.com>
2023-07-01 08:29:34 +0530

    models & CRUD

--- util/database.js
+++ util/database.js
@@ -1,15 +1,27 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
-const MongoConnect = callback => {
-    MongoClient.connect(process.env.DB)
-        .then(client => {
-            console.log('Database connected');
-            callback(client);
-        })
-        .catch(err => {
-            console.log(err);
-        });
+let _db;
+
+const mongoConnect = callback => {
+  MongoClient.connect(process.env.DB)
+    .then(client => {
+      console.log('Connected!');
+      _db = client.db();
+      callback();
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
 };
 
-module.exports = MongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
